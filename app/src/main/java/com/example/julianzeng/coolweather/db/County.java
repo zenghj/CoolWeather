21c2d63b81717f928a9c1919bcaa8770d3fcbf46
Julianzeng <zenghengjun@xiaomi.com>
2018-10-16 15:51:57 +0800

    加入创建数据库和表的各配置

---
+++ app/src/main/java/com/example/julianzeng/coolweather/db/County.java
@@ -0,0 +1,43 @@
+package com.example.julianzeng.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
+
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+}
